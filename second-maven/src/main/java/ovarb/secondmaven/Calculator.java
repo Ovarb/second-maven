f189cc3d46bc118d90087a2c040591fbc6957b08
ovarb <ovarb6@gmail.com>
2022-05-31 03:49:14 +0300

    add calc class

---
+++ second-maven/src/main/java/ovarb/secondmaven/Calculator.java
@@ -0,0 +1,14 @@
+package ovarb.secondmaven;
+
+public class Calculator {
+	
+	public void calcprint (int a) {
+		System.out.println("Calculated = " + a);
+	}
+
+	public int calcadd (int a, int b) {
+		
+		int c = a + b;
+		return c;
+	}
+}
--- second-maven/src/main/java/ovarb/secondmaven/HelloMaven2.java
+++ second-maven/src/main/java/ovarb/secondmaven/HelloMaven2.java
@@ -5,6 +5,14 @@ public class HelloMaven2 {
 	public static void main(String[] args) {
 		System.out.println("Hello Maven1!");
 
+		int a = 1;
+		int b = 2;
+		int c;
+		
+		Calculator calc = new Calculator();
+		c = calc.calcadd(a, b);
+		calc.calcprint(c);
+		
 	}
 
 }
